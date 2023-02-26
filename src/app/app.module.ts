5272690789ff59d257915176f66a47b9cd473181
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2023-02-25 23:32:24 -0500

    feat: chat view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VoiceButtonComponent } from './components/voice-button/voice-button.component';
+import { ConversationComponent } from './components/conversation/conversation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VoiceButtonComponent
+    VoiceButtonComponent,
+    ConversationComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/voice-button/voice-button.component.ts
+++ src/app/components/voice-button/voice-button.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class VoiceButtonComponent {
 
+  record: boolean = false;
+  recorder: any;
+
+  recording(){
+    this.record = !this.record;
+
+    if (!this.recorder) {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then((stream) => {
+          this.recorder = new MediaRecorder(stream);
+          this.recorder.start();
+
+          this.recorder.ondataavailable = (event: { data: any; }) => {
+            const blob = event.data;
+            const file = new File([blob], 'recording.mp3', { type: 'audio/mp3' });
+
+            console.log(file); // Aquí puedes enviar el archivo al backend
+          }
+        });
+    } else {
+      this.recorder.stop();
+      this.recorder = null;
+
+      // Detener la fuente de audio para que no siga grabando
+      const tracks = this.recorder.stream.getTracks();
+      tracks.forEach((track: { stop: () => any; }) => track.stop());
+    }
+  }
+
+
 }

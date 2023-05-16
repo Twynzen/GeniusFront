1200835f7e5db6402fdb9102aedf216da9505b63
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2023-05-16 18:41:09 -0500

    feat: add music

--- src/app/components/alarm/alarm.component.ts
+++ src/app/components/alarm/alarm.component.ts
@@ -16,8 +16,13 @@ export class AlarmComponent implements OnInit {
   });
   alarmTime: string | null = '';
   alarms: string[] = [];
+  private alarmSound: any;
 
-  constructor() {}
+  constructor() {
+    this.alarmSound = new Audio();
+    this.alarmSound.src = 'assets/sounds/alarm.mp3'; // Asegúrate de que esta ruta apunta a tu archivo de audio
+    this.alarmSound.load();
+  }
 
   ngOnInit() {
     // Actualiza la hora cada segundo
@@ -28,31 +33,53 @@ export class AlarmComponent implements OnInit {
   }
   getCurrentTime(): string {
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
+    let hours = now.getHours();
     const minutes = now.getMinutes().toString().padStart(2, '0');
-    return `${hours}:${minutes}`;
+    const midday = hours >= 12 ? 'PM' : 'AM';
+
+    // Convertir a formato de 12 horas
+    hours = hours % 12;
+    // La hora '0' debe ser '12'
+    hours = hours ? hours : 12;
+
+    return `${hours.toString().padStart(2, '0')}:${minutes} ${midday}`;
+  }
+
+  playAlarmSound() {
+    this.alarmSound.play();
   }
 
   setAlarm() {
-    this.alarmTime = this.alarmForm.get('alarm')?.value;
-    if (this.alarmTime) {
-      this.alarms.push(this.alarmTime);
+    let alarmTime = this.alarmForm.get('alarm')?.value;
+
+    if (alarmTime) {
+      // Convertir la hora de la alarma a formato de 12 horas
+      let [hours, minutes] = alarmTime.split(':');
+      hours = parseInt(hours);
+      const midday = hours >= 12 ? 'PM' : 'AM';
+      hours = hours % 12;
+      hours = hours ? hours : 12;
+      alarmTime = `${hours.toString().padStart(2, '0')}:${minutes} ${midday}`;
+
+      this.alarms.push(alarmTime);
     }
+
     this.alarmForm.reset();
   }
 
   checkAlarm() {
     this.alarms = this.alarms.filter((alarm) => {
       if (alarm === this.clock) {
-        console.log('hola');
-
         this.showAlarmPopup(alarm);
         return false;
       }
       return true;
     });
   }
+
   showAlarmPopup(time: string) {
+    this.playAlarmSound();
+
     Swal.fire({
       title: '¡Alarma!',
       html: `<span class="text-white">La alarma establecida para las ${time} está sonando.</span>`,
